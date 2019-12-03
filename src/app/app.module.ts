02a397dd13e4b143d6bd2bd7dc6cac1325119adb
richarddevers <ritchiedev25@gmail.com>
2019-12-03 16:28:46 +0100

    add routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { MatGridListModule } from "@angular/material/grid-list";
 import { MatInputModule } from "@angular/material/input";
 import { MatSelectModule } from "@angular/material/select";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from "@angular/forms";
+
+import { AppRoutingModule } from "./app-routing.module";
 
 import { CurtainsComponent } from "./curtains/curtains.component";
 import { InputsComponent } from "./inputs/inputs.component";
-import { ParentComponent } from './parent/parent.component';
-import {MatDividerModule} from '@angular/material/divider';
+import { ParentComponent } from "./parent/parent.component";
+import { MatDividerModule } from "@angular/material/divider";
 
 @NgModule({
   imports: [
@@ -23,9 +25,15 @@ import {MatDividerModule} from '@angular/material/divider';
     MatInputModule,
     MatSelectModule,
     BrowserAnimationsModule,
-    MatDividerModule
+    MatDividerModule,
+    AppRoutingModule
   ],
-  declarations: [AppComponent, CurtainsComponent, InputsComponent, ParentComponent],
+  declarations: [
+    AppComponent,
+    CurtainsComponent,
+    InputsComponent,
+    ParentComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
