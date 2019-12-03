dab9ed94cba71032093828cc695986b22047f732
richarddevers <ritchiedev25@gmail.com>
2019-12-03 14:32:11 +0100

    add space number input

--- src/app/inputs/inputs.component.ts
+++ src/app/inputs/inputs.component.ts
@@ -15,24 +15,37 @@ import {
 })
 export class InputsComponent implements OnInit {
   @Output() plisNumber = new EventEmitter<number>();
+  @Output() spaceNumber = new EventEmitter<number>();
   @Output() taillePlis = new EventEmitter<number>();
   @Output() largeurBrute = new EventEmitter<number>();
   @Output() devanture = new EventEmitter<number>();
   @Output() retour = new EventEmitter<number>();
 
+  private plisNumberView:number = DEFAULT_NOMBRE_PLIS;
+  private spaceNumberView:number = DEFAULT_NOMBRE_ESPACE;
+
   private DEFAULT_LARGEUR=DEFAULT_LARGEUR;
   private DEFAULT_RETOUR=DEFAULT_RETOUR;
   private DEFAULT_DEVANTURE=DEFAULT_DEVANTURE;
-  private DEFAULT_NOMBRE_PLIS=DEFAULT_NOMBRE_PLIS;
   private DEFAULT_TAILLE_PLIS=DEFAULT_TAILLE_PLIS;
-  private DEFAULT_NOMBRE_ESPACE=DEFAULT_NOMBRE_ESPACE;
-  constructor() {}
 
-  ngOnInit() {
+  constructor() { }
+
+  ngOnInit() { }
+
+
+  setPlis(i: string) {
+    this.plisNumber.emit(+i);
+    this.spaceNumber.emit(+i-1)
+    this.plisNumberView=+i;
+    this.spaceNumberView=+i-1;
   }
 
-  changePlis(i: string) {
-    this.plisNumber.emit(+i);
+  setSpace(i: string) {
+    this.spaceNumber.emit(+i)
+    this.plisNumber.emit(+i+1);
+    this.spaceNumberView=+i;
+    this.plisNumberView=+i+1;
   }
 
   setTaillePlis(i: string) {
