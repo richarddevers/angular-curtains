14bbafbd5701fe9d024230c5b82750d1658f62b9
richarddevers <ritchiedev@hotmail.com>
2019-12-09 16:38:37 +0100

    add space size as input

--- src/app/inputs/inputs.component.ts
+++ src/app/inputs/inputs.component.ts
@@ -5,7 +5,7 @@ import {
   DEFAULT_DEVANTURE,
   DEFAULT_NOMBRE_PLIS,
   DEFAULT_TAILLE_PLIS,
-  DEFAULT_NOMBRE_ESPACE
+  DEFAULT_NOMBRE_ESPACE,
 } from "../default";
 
 @Component({
@@ -19,29 +19,51 @@ export class InputsComponent implements OnInit {
   @Output() largeurBrute = new EventEmitter<number>();
   @Output() devanture = new EventEmitter<number>();
   @Output() retour = new EventEmitter<number>();
-
-  plisNumberView: number = DEFAULT_NOMBRE_PLIS;
-  spaceNumberView: number = DEFAULT_NOMBRE_ESPACE;
+  @Output() largeurEspace = new EventEmitter<number>();
 
   DEFAULT_LARGEUR: number = DEFAULT_LARGEUR;
   DEFAULT_RETOUR: number = DEFAULT_RETOUR;
   DEFAULT_DEVANTURE: number = DEFAULT_DEVANTURE;
   DEFAULT_TAILLE_PLIS: number = DEFAULT_TAILLE_PLIS;
+  
+  plisNumberView: number = DEFAULT_NOMBRE_PLIS;
+  spaceNumberView: number = DEFAULT_NOMBRE_ESPACE;
+  largeurBruteView: number = +DEFAULT_LARGEUR;
+  largeurEspaceView: number = +(DEFAULT_LARGEUR / DEFAULT_NOMBRE_ESPACE).toFixed(2);
 
-  constructor() {}
+  constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
+
+  setLargeurEspace(i: string){
+    const largeurEspace = +((+i).toFixed(2));
+    const largeurBrute = +((largeurEspace * this.spaceNumberView).toFixed(2));
+
+    this.largeurBruteView = +((largeurEspace * this.spaceNumberView).toFixed(2));
+    this.largeurBrute.emit(largeurBrute);
+
+    this.largeurEspaceView = largeurEspace;
+    this.largeurEspace.emit(largeurEspace);
+  }
 
   setPlis(i: string) {
     this.plisNumber.emit(+i);
     this.plisNumberView = +i;
+
     this.spaceNumberView = +i - 1;
+    
+    this.largeurBrute.emit(this.getLargeurBrute());
+    this.largeurBruteView = this.getLargeurBrute();
   }
 
   setSpace(i: string) {
-    this.plisNumber.emit(+i + 1);
     this.spaceNumberView = +i;
+
+    this.plisNumber.emit(+i + 1);
     this.plisNumberView = +i + 1;
+
+    this.largeurBrute.emit(this.getLargeurBrute());
+    this.largeurBruteView = this.getLargeurBrute();
   }
 
   setTaillePlis(i: string) {
@@ -50,6 +72,11 @@ export class InputsComponent implements OnInit {
 
   setLargeurBrute(i: string) {
     this.largeurBrute.emit(+i);
+    this.largeurBruteView = +i;
+
+    const largEspace = +((+i/this.spaceNumberView).toFixed(2));
+    this.largeurEspaceView = largEspace;
+    this.largeurEspace.emit(largEspace);
   }
 
   setDevanture(i: string) {
@@ -58,6 +85,10 @@ export class InputsComponent implements OnInit {
 
   setRetour(i: string) {
     this.retour.emit(+i);
+  }
+
+  getLargeurBrute():number{
+    return +((this.spaceNumberView * this.largeurEspaceView).toFixed(2));
   }
 
 }
